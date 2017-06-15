d5cf8e488120678d4911ea9d756704128cb71c29
Oliver <soliver@usgs.gov>
2017-06-15 15:22:25 -0500

    created discrete task for getting data out of aqualog

---
+++ scripts/0_getdata/getdata_aqualog.R
@@ -0,0 +1,10 @@
+# source revised formatAbsSamples function
+source("scripts/0_getdata/formatAbsSamplesRevised.R")
+
+# collate data
+FinalAbsDf <- formatAbsSamplesRevised(dateLower='20130930',dateUpper='20170113',Type='All',Project='GMIA')
+
+# remove columns with NA names
+FinalAbsDf2 <- FinalAbsDf[,names(FinalAbsDf) != "NA"]
+
+write.csv(FinalAbsDf2, "raw_data/absorbance/rawCompiledAbs.csv", row.names = FALSE)
