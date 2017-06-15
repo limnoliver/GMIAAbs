fd485d1826719a605002a91b6fdf6b02535d09ba
Oliver <soliver@usgs.gov>
2017-06-15 15:23:15 -0500

    created discrete task for cleaning up data from aqualog and getting summary coefficients from getAbs

---
+++ scripts/2_process/calcAbsCoef.R
@@ -0,0 +1,30 @@
+# this script cleans data from
+# formatAbsSamplesRevised function
+# gets summary data from getAbs
+# saves summarized data
+library(USGSHydroOpt)
+
+source("/scripts/getdata_aqualog.R")
+
+rawAbs <- read.csv("raw_data/absorbance/rawCompiledAbs.csv")
+
+#clean up column names
+testnames <- colnames(rawAbs)
+testnames <- gsub("USGS","Group",testnames)
+colnames(rawAbs) <- testnames
+
+# get rid of last column name that is wavelengths
+testnames <- testnames[-length(testnames)]
+
+# make a data frame that will become summary table for getAbs function
+test <- data.frame(testnames,stringsAsFactors=FALSE)
+
+colnames(test) <- "GRnumber"
+wavs <- unique(rawAbs$Wavelength)
+wavs <- wavs[which(wavs<=700)]
+
+# get rid of NA values 
+testAbs <- getAbs(rawAbs,'Wavelength',wavs,"Group",test,"GRnumber")
+
+# write data to cached data fikder
+write.csv(testAbs, "cached_data/SummaryAbsCoef.csv", row.names = FALSE)
