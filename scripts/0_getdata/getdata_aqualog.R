ef0b7be618167b3df067bfd9725373c00dbdd681
Oliver <soliver@usgs.gov>
2017-06-16 13:43:24 -0500

    Added filter on data to this script to save filtered data as csv

--- scripts/0_getdata/getdata_aqualog.R
+++ scripts/0_getdata/getdata_aqualog.R
@@ -5,6 +5,11 @@ source("scripts/0_getdata/formatAbsSamplesRevised.R")
 FinalAbsDf <- formatAbsSamplesRevised(dateLower='20130930',dateUpper='20170113',Type='All',Project='GMIA')
 
 # remove columns with NA names
-FinalAbsDf2 <- FinalAbsDf[,names(FinalAbsDf) != "NA"]
+FinalAbsDf <- FinalAbsDf[,names(FinalAbsDf) != "NA"]
+
+# Filter out specific column names
+finalcols <- colnames(FinalAbsDf)
+finalcols <- finalcols[which(substr(finalcols,1,2) %in% c("OU","Ou","CG","LK","OA","Wa"))]
+FinalAbsDf <- FinalAbsDf[,finalcols]
 
 write.csv(FinalAbsDf2, "raw_data/rawCompiledAbs.csv", row.names = FALSE)
