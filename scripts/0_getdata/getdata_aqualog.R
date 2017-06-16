3ebd30e217c193afbd7e942c1ed99641fc790157
Oliver <soliver@usgs.gov>
2017-06-16 14:49:34 -0500

    took out prefiltering before writing csv

--- scripts/0_getdata/getdata_aqualog.R
+++ scripts/0_getdata/getdata_aqualog.R
@@ -7,9 +7,4 @@ FinalAbsDf <- formatAbsSamplesRevised(dateLower='20130930',dateUpper='20170113',
 # remove columns with NA names
 FinalAbsDf <- FinalAbsDf[,names(FinalAbsDf) != "NA"]
 
-# Filter out specific column names
-finalcols <- colnames(FinalAbsDf)
-finalcols <- finalcols[which(substr(finalcols,1,2) %in% c("OU","Ou","CG","LK","OA","Wa"))]
-FinalAbsDf <- FinalAbsDf[,finalcols]
-
-write.csv(FinalAbsDf2, "raw_data/rawCompiledAbs.csv", row.names = FALSE)
+write.csv(FinalAbsDf, "raw_data/rawCompiledAbs.csv", row.names = FALSE)
