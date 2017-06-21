4c2d3cd7d725f84d6d7b40aaf0e6e1606eb4d94f
Oliver <soliver@usgs.gov>
2017-06-21 16:17:50 -0500

    removed cleaning steps from importing raw data

--- scripts/getdata_waterquality.R
+++ scripts/getdata_waterquality.R
@@ -1,45 +1,9 @@
 library(readxl)
 
+# read in latest wq file -- this reads in 2017 version
 wq_data <- read_xlsx('M:/NonPoint Evaluation/gmia/WY2017 QW data/WY 2017 water-quality by site.xlsx', sheet = 'qw', skip = 1)
 
+# write tab "qw" as csv
 write.csv(wq_data, "raw_data/rawWQdata.csv", row.names = FALSE)
 
-names(wq_data)[151] <- 'ProjectID'
-which(GMIASag$ProjectID %in% wq_data$ProjectID)
 
-# filter wq data
-
-# keep only rows that have a project ID
-wq_filtered <- wq_data %>%
-  filter(!is.na(ProjectID))
-
-# format ProjectID to look like the GMIASag$ProjectID
-wq_filtered$ProjectID <- gsub('-', '.', wq_filtered$ProjectID)
-
-# filter which WQ variables are of interest
-
-vars.keep <- grep("cod,|bod|formate|acetate|glycol|calcium|magnesium|potassium|sodium|NH3|ammonia|NO2|ethylene|propylene|methyl", names(wq_filtered), ignore.case = TRUE)
-vars.keep <- c(vars.keep, vars.keep-1)
-vars.keep <- sort(vars.keep)
-
-wq_filtered <- wq_filtered[,c(151, 1:4, vars.keep)]
-
-# clean up names of columns
-# including turning "remark" columns into 
-# "rVariable" so they are identifiable by name
-
-new.names <- names(wq_filtered)
-# remove anything after first comma in wq var names
-new.names <- gsub("(\\w),( .*)", "\\1",new.names)
-# match remarks to variable names
-for (i in 1:length(new.names)){
-  if (length(grep("Remark", new.names[i]) == 1)) {
-    new.names[i] <- paste("r", new.names[i+1], sep = "")
-  }
-}
-# get rid of all spaces in column names, 
-# turn into underscores
-new.names <- gsub("\\s", "_", new.names)
-
-# rename columns in wq dataframe
-names(wq_filtered) <- new.names
