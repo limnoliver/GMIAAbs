db1a2ad875198002529ed0252bc0f8475e4ae71e
Oliver <soliver@usgs.gov>
2017-06-21 11:23:16 -0500

    removed commands that belong in absResidual

--- scripts/2_process/absSlopes.R
+++ scripts/2_process/absSlopes.R
@@ -1,38 +1,4 @@
-library(USGSHydroOpt)
-testAbsWorking <- read.csv('cached_data/AbsData.csv')
-FinalAbsDf <- read.csv('cached_data/SummarizedAbsData.csv')
-# calculate residuals for selected absorbence ranges
-#source("/Users/jlthomps/Desktop/git/GMIA/getExpResidJT.R")
-wavelength <- 491
-rangeReg <- c(419,602)
-rangeGap <- c(461,521)
-colsAbs <- unique(testAbsWorking$GRnumber)
-#colsAbs <- colsAbs[-which(substr(colsAbs,1,1)=='Q')]
-colsAbs <- c(as.character(colsAbs),"Wavelength")
-dataAbs <- FinalAbsDf[,colsAbs]
-waveCol <- "Wavelength"
-colSubsetString <- "Gr"
-dataSummary <- testAbsWorking
-grnum <- "GRnumber"
-testdfOpt <- getExpResid(wavelength,rangeReg,rangeGap,dataAbs,waveCol,colSubsetString,dataSummary,grnum)
-
-wavelength <- 629
-rangeReg <- c(560,698)
-rangeGap <- c(605,662)
-dataSummary <- testdfOpt
-testdfOpt2 <- getExpResid(wavelength,rangeReg,rangeGap,dataAbs,waveCol,colSubsetString,dataSummary,grnum)
-cols2 <- colnames(testdfOpt2)
-cols2 <- cols2[1:158]
-cols2 <- c(cols2,"Resids490","Resids630")
-colnames(testdfOpt2) <- cols2
-
-wavelength <- 422
-rangeReg <- c(377,497)
-rangeGap <- c(404,452)
-dataSummary <- testdfOpt2
-testdfOpt3 <- getExpResid(wavelength,rangeReg,rangeGap,dataAbs,waveCol,colSubsetString,dataSummary,grnum)
-names(testdfOpt3)[length(testdfOpt3)] <- "Resids422"
-
+# script that uses output from absResiduals, and adds spectral slopes
 
 # calculate slopes for selected absorbence values
 sag <- read.csv("SagVals.csv",stringsAsFactors=FALSE)
