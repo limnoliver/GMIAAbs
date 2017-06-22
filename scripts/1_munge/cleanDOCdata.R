308c25f693fb63ce43727c09bfd87f425efeda42
Oliver <soliver@usgs.gov>
2017-06-22 13:50:02 -0500

    Figure out what to do with repeated ProjectIDs - could be machine rep, field rep, or sample rerun.

--- scripts/1_munge/cleanDOCdata.R
+++ scripts/1_munge/cleanDOCdata.R
@@ -7,7 +7,37 @@ doc.raw <- read.csv('raw_data/rawDOCdata.csv')
 rows.exclude <- grep('blank*|ppm|unknown|standard*|std|dilution', doc.raw$ProjectID, ignore.case = TRUE)
 doc.cleaned <- doc.raw[-rows.exclude, ]
 
-# get rows with no info in them, either ProjectID or DOC
+# get rid of rows with no info in them, either ProjectID or DOC
 doc.cleaned <- doc.cleaned[!is.na(doc.cleaned$DOC), ]
 doc.cleaned <- doc.cleaned[!is.na(doc.cleaned$ProjectID), ]
 
+#########################################
+# get rid of duplicate information
+# several reasons why a sample ID might be repeated
+# 1) machine replicates that measure the same aliquot of water, and the means of these are exported for
+# every sample ID - sample ID exactly the same. SOLUTION: take one of the values, or mean (same answer)
+# 2) field replicates where two aliquots were collected at same site/time, 
+# these have "-rep" or "-REP" added to the end. SOLUTION: take mean
+# 3) sample reruns - where same water was run on a later date due to measurement errors/issues,
+# these are likely to have same sample id but different date. SOLUTION: take lastest measurement, or 
+# sample from file with latest date field
+
+# 1) get rid of rows that are exactly the same across all columns
+doc.unique <- unique(doc.cleaned)
+
+# 2) identify field replicates and take mean of sample + rep
+reps <- grep('rep|-R', doc.unique$ProjectID, ignore.case = TRUE)
+rep.names <- doc.unique$ProjectID[grep('rep|-R', doc.unique$ProjectID, ignore.case = TRUE)]
+new.names <- gsub('\\(rep\\)|-REP|-R', "", rep.names, ignore.case = FALSE)
+
+doc.reps <- doc.unique[reps,]
+doc.reps$NewNames <- new.names
+doc.unique2 <- doc.unique[-reps, ]
+
+for (i in 1:nrow(doc.unique2)){
+  if (doc.unique2$ProjectID[i] %in% new.names) {
+    doc.unique2$DOC[i] <- mean(doc.unique2$DOC[i], doc.reps$DOC[which(doc.reps$NewNames %in% doc.unique2$ProjectID[i])])
+  }
+}
+
+# 3) now find duplicated ProjectIDs that have different dates
