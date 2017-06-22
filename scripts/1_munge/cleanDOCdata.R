e4d27c07b890bdaf33b32173e3ae19ef887d0de7
Oliver <soliver@usgs.gov>
2017-06-22 12:09:50 -0500

    remove rows with NAs, remove QA samples

--- scripts/1_munge/cleanDOCdata.R
+++ scripts/1_munge/cleanDOCdata.R
@@ -1,2 +1,13 @@
 # this script cleans up DOC data
 
+doc.raw <- read.csv('raw_data/rawDOCdata.csv')
+
+# identify QA rows from 'ProjectID' column
+# these include blanks and standards
+rows.exclude <- grep('blank*|ppm|unknown|standard*|std|dilution', doc.raw$ProjectID, ignore.case = TRUE)
+doc.cleaned <- doc.raw[-rows.exclude, ]
+
+# get rows with no info in them, either ProjectID or DOC
+doc.cleaned <- doc.cleaned[!is.na(doc.cleaned$DOC), ]
+doc.cleaned <- doc.cleaned[!is.na(doc.cleaned$ProjectID), ]
+
