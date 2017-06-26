b1170d4fb94185de3ee9334dff74ddf881ebe12f
Oliver <soliver@usgs.gov>
2017-06-26 14:36:02 -0500

    added filter to get rid of replicate abs data. Move this to munging later on.

--- scripts/2_process/mergeWQ_DOC_ABS.R
+++ scripts/2_process/mergeWQ_DOC_ABS.R
@@ -7,4 +7,10 @@ Abs <- read.csv('cached_data/absSlopesResiduals.csv')
 all.dat <- merge(Abs, DOC, 'ProjectID', all.x = TRUE)
 all.dat <- merge(all.dat, WQ, 'ProjectID', all.x = TRUE)
 
+# drop ProjectID rows with .R
+# fix this in absorbance import later
+
+reps <- grep('.r', all.dat$ProjectID, ignore.case = TRUE)
+all.dat <- all.dat[-reps, ]
+
 write.csv(all.dat, "cached_data/mergedWQ_DOC_ABS.csv", row.names = FALSE)
