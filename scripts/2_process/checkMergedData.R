247c06d93033e7b2edc900e4aecd73887bbb98ad
Oliver <soliver@usgs.gov>
2017-06-28 16:12:27 -0500

    check absorbance event ids with that in sample log. Find which ones are missing and reasons for missing.

--- scripts/2_process/checkMergedData.R
+++ scripts/2_process/checkMergedData.R
@@ -1,8 +1,19 @@
-
+dat.all <- read.csv('cached_data/mergedWQ_DOC_ABS.csv')
+all.dat <- dat.all
 # find missing COD data (should have the WQ samples)
 dat.all <- all.dat
 missing <- all.dat[is.na(all.dat$COD),]
 missing.DOC <- all.dat[is.na(all.dat$DOC), ]
+
+# verify that all storm events from optics.sample.log.ALL are in the 
+# pulled data
+
+sample.log <- read_xlsx('M:/NonPoint Evaluation/gmia/SLOH labforms and budget/optics.sample.log.ALL.xlsx')
+sample.log$Storm <- gsub("-", ".", sample.log$Storm)
+samples.in <- which(sample.log$Storm %in% dat.all$ProjectID)
+samples.out <- sample.log[-samples.in, ]
+upstream <- grep('US.', samples.out$Storm)
+samples.out <- samples.out[-upstream, ]
 
 # data checks
 library(RColorBrewer)
@@ -13,9 +24,6 @@ palette(colorRampPalette(c("pink", "red", "purple", "purple4"))(22))
 plot(log10(all.dat$COD)~log10(all.dat$DOC), col = levels(as.factor(all.dat$date)), pch = 16)
 legend("topleft", legend = levels(as.factor(all.dat$date)), pch = 16, col = levels(as.factor(all.dat$date)))
 
-library(caret)
-
-featurePlot()
 
 # look at WQ data
 
@@ -48,3 +56,8 @@ png('figures/COD_exp_obs.png', height = 500, width = 500)
 plot(log10(wq.data$COD) ~ log10(expectedCOD), xlab = "Expected COD", ylab = "Oberved COD")
 abline(0,1,col = "red")
 dev.off()
+
+# Look into DOC outliers
+DOC.outliers <- subset(log.wq, log.wq$COD < 2.2 & log.wq$DOC > 1.5 & log.wq$DOC <2.2)
+outliers <- c(91,112,114,193)
+wq.data.outs <- all.dat[which(rownames(all.dat) %in% as.character(outliers)), ]
