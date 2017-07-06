4e24c54fa3d73643b66cade02b15702ef07f37f9
Oliver <soliver@usgs.gov>
2017-07-06 09:43:33 -0500

    added code to calculate MRL and correct absorbance values. Also plots number of censored values per wavelength.

--- scripts/2_process/correctAbs_MRL.R
+++ scripts/2_process/correctAbs_MRL.R
@@ -1,10 +1,48 @@
-# find the minimum reporting limit from field blanks
+# this code finds the minimum reporting limit from field blanks
+# and corrects absorbances for MRLs
+
+# source Steve's code to summarize blanks
+source('scripts/2_process/fxn_absMRL.R')
+
+# source Steve's code to correct absorbance values
+source('scripts/2_process/fxn_absMRLAdjust.R')
+
+# read in raw absorbance data
 abs.raw <- read.csv('raw_data/rawCompiledAbs.csv')
+
+# calculate MRL based on all blank samples
+blankGRnums.all <- grep('^Q[[:alpha:]]', names(abs.raw), value = TRUE)
+MRL.all <- absMRL(abs.raw, "Wavelength", blankGRnums.all)
+
+# read in cleaned absorbance data
+abs.cleaned <- read.csv("cached_data/SummarizedAbsData.csv")
+wl.column <- grep('Wavelength', names(abs.cleaned))
+abs.cleaned <- abs.cleaned[,c(wl.column, 1:(wl.column-1))]
+GRnums <- names(abs.cleaned)[2:length(abs.cleaned)]
+
+# adjust values from MRL - here setting to 1/2 MRL
+# function outputs two dataframes - one with adjusted values, the other with "<" for all corrected values
+abs.corrected <- absMRLAdjust(dfabs = abs.cleaned, dfMRLs = MRL.all, Wavelength = 'Wavelength', sampleGRnums = GRnums, multiplier = 0.5)
+
+# find out how many censored values we have for each wavelength to determine which wavelengths we should use
+# turn "<" into NAs
+# maybe add this to function in the future
+for (i in 2:length(abs.censored)){
+  abs.censored[,i] <- as.numeric(as.character(abs.censored[,i]))
+}
+# count NAs per row (# of samples censored per wavelength)
+count_na <- function(x) sum(is.na(x))
+abs.censored$n_censored <- apply(abs.censored, 1, count_na)
+abs.censored$prop_censored <- abs.censored$n_censored/(length(abs.censored)-1)
+
+png('figures/Abs_prop_censored.png')
+plot(prop_censored~Wavelength, data = abs.censored, ylab = "Proportion of Samples < MDL", cex.lab = 1.3)
+dev.off()
+
 
 # test if blanks by site makes a difference
 # that is, is there contamination at any sites?
 
-blankGRnums.all <- grep('^Q[[:alpha:]]', names(abs.raw), value = TRUE)
 blankGRnums.OUT <- grep('QOUT', names(abs.raw), value = TRUE)
 blankGRnums.LK <- grep('QLK', names(abs.raw), value = TRUE)
 blankGRnums.CG <- grep('QCG', names(abs.raw), value = TRUE)
@@ -16,6 +54,7 @@ data.list[[2]] <- absMRL(abs.raw, "Wavelength", blankGRnums.OUT)
 data.list[[3]] <- absMRL(abs.raw, "Wavelength", blankGRnums.LK)
 data.list[[4]] <- absMRL(abs.raw, "Wavelength", blankGRnums.CG)
 data.list[[5]] <- absMRL(abs.raw, "Wavelength", blankGRnums.US)
+
 
 
 # visualize data for major differences
