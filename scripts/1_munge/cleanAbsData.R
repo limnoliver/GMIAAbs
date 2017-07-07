130a904446bed741924da54151e261ff92411588
Oliver <soliver@usgs.gov>
2017-07-07 12:53:30 -0500

    get rid of samples that have a duplicate sample ID but also have a wrong date. May not need this after I fix abs import duplicate issues.

--- scripts/1_munge/cleanAbsData.R
+++ scripts/1_munge/cleanAbsData.R
@@ -1,3 +1,4 @@
+library(readxl)
 # read in transposed absorbance data where each wavelength has own column
 abscoef <- read.csv('cached_data/transposedAbsCoef.csv', header = TRUE)
 
@@ -29,12 +30,47 @@ samples.keep <- samples.keep[-grep('dil|dup',samples.keep, ignore.case = TRUE)]
 # get rid of random samples that made it through that have long names - think these were runs of the deicers themselves
 samples.keep <- samples.keep[-which(nchar(samples.keep)>12)]
 
+# get rid fo samples that have "Q" after the dash or decimal
+samples.keep <- samples.keep[-grep('\\.Q', samples.keep)]
+
+# filter abscoef
+abscoef.f <- abscoef[abscoef$ProjectID %in% samples.keep, ] 
+
+# find all unique project ids
+unique.PID <- unique(abscoef.f$ProjectID)
+
 # some duplicate Project IDs left - pull in optics sample log to verify we are using the correct date for each sample
-samples.compare <- abscoef[which(abscoef$ProjectID %in% samples.keep), ]
+
 sample.log <- read_xlsx('M:/NonPoint Evaluation/gmia/SLOH labforms and budget/optics.sample.log.ALL.xlsx')
+sample.log$ProjectID <- gsub('-', '.', sample.log$ProjectID)
+
+
+# create a column to populate with decision to remove or not based 
+# on wrong date assessment
+abscoef.f$remove <- FALSE
+
+for (i in 1:nrow(abscoef.f)){
+  if (length(grep(paste('^',abscoef.f$ProjectID[i],'$', sep = ""), sample.log$ProjectID, ignore.case = TRUE)) == 0) {
+    abscoef.f$remove[i] = FALSE
+  } else {
+    row.num <- grep(paste('^',abscoef.f$ProjectID[i],'$', sep = ""), sample.log$ProjectID, ignore.case = TRUE)
+    log <- sample.log[row.num, ]
+    if (length(grep(paste('^',abscoef.f$ProjectID[i],'$', sep = ""), abscoef.f$ProjectID)) > 1 & abscoef.f$date[i] != log$`Optics processed date`){
+      abscoef.f$remove[i] = TRUE
+  }
+ 
+  }
+}
+
+# filter out duplicated samples with the wrong date
+
+abscoef.f <- abscoef.f[abscoef.f$remove == FALSE, ]
+
+# do comparison
+samples.compare <- abscoef.f
 samples.compare <- samples.compare[,c('date', 'ProjectID')]
 names(sample.log)[2] <- 'ProjectID'
-sample.log$ProjectID <- gsub('-', '.', sample.log$ProjectID)
+
 
 comparison <- merge(sample.log[,c(2,7)], samples.compare, all.x = TRUE)
 
@@ -49,6 +85,15 @@ for (i in 1:nrow(comparison)){
     comparison$test[i] <- 'wrong date'
   }
 }
+
+comparison$duplicated <- duplicated(comparison$ProjectID)
+
+
+# for samples with double entries, look at the optics log and pick correct date
+
+
+
+
 # find site-specific duplicates
 abscoefOAK <- abscoef[grep("OAK",abscoef$ProjectID),]
 # remove duplicate sample for OAK site
