f003bc208225653255b33fd84291fec7d22fa843
Oliver <soliver@usgs.gov>
2017-07-31 15:55:21 -0500

    write project working directory before getting aqualog data so does not disrupt wd of other functions

--- scripts/0_getdata/getdata_aqualog.R
+++ scripts/0_getdata/getdata_aqualog.R
@@ -1,10 +1,13 @@
 # source revised formatAbsSamples function
 source("scripts/0_getdata/formatAbsSamplesRevised.R")
 
+# set home wd
+wd.home <- getwd()
 # collate data
 FinalAbsDf <- formatAbsSamplesRevised(dateLower='20130930',dateUpper='20170610',Type='All',Project='GMIA')
 
 # remove columns with NA names
 FinalAbsDf <- FinalAbsDf[,names(FinalAbsDf) != "NA"]
 
+setwd(wd.home)
 write.csv(FinalAbsDf, "raw_data/rawCompiledAbs.csv", row.names = FALSE)
