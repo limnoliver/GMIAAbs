ec096335e49142e42a6a28f823816322c20dee40
Oliver <soliver@usgs.gov>
2017-08-01 10:22:53 -0500

    updated input data for slope calculation

--- scripts/2_process/absSlopes.R
+++ scripts/2_process/absSlopes.R
@@ -2,7 +2,7 @@
 # read in dataframe that has residuals calculated
 # import processed data
 absResiduals <- read.csv('cached_data/absResiduals.csv')
-FinalAbsDf <- read.csv("cached_data/SummarizedAbsData.csv")
+dataAbs <- read.csv('cached_data/correctedAbsData.csv')
 
 # read in selected absorbence values
 sag <- read.csv("raw_data/SagVals.csv",stringsAsFactors=FALSE)
@@ -12,6 +12,6 @@ colSubsetString <- "Gr"
 dataSummary <-  absResiduals
 grnum <- "GRnumber"
 
-GMIASag <- getSag(FinalAbsDf,waveCol,sag,colSubsetString,dataSummary,"GRnumber")
+GMIASag <- getSag(dataAbs,waveCol,sag,colSubsetString,dataSummary,"GRnumber")
 
 write.csv(GMIASag, "cached_data/absSlopesResiduals.csv", row.names = FALSE)
