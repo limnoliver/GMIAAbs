421388017adfbad0209e9030c60390b89d8d6841
Oliver <soliver@usgs.gov>
2017-08-01 11:44:46 -0500

    added script to filter out upstream and summer observations. Also removes abs WL > 500 due to high proportion of censored values.

---
+++ scripts/2_process/filter_data.R
@@ -0,0 +1,29 @@
+# filter data for modeling
+# this includes getting rid of upstream and summer events
+# also includes filtering out wavelengths over 500 (not a lot of information, many values < MDL)
+
+all.dat <- read.csv('cached_data/mergedWQ_DOC_ABS.csv', stringsAsFactors = FALSE)
+
+# ID upstream events
+us <- grep('US', all.dat$ProjectID)
+
+# get rid of US events
+filt.dat <- all.dat[-us, ]
+
+# ID summer events
+month <- substr(filt.dat$date, start = 5, stop = 6)
+summer.rows <- grep('05|06|07|08|09|10', month)
+
+# get rid of summer events
+filt.dat <- filt.dat[-summer.rows,]
+
+# get rid of WL > 500 as WL > 500 have 
+# high proportion (>30%) of observations that are censored
+# (< MDL) -- see Abs_prop_censored.png
+
+start.abs <- grep('A800', names(filt.dat))
+end.abs <- grep('A503', names(filt.dat))
+filt.dat <- filt.dat[,-(start.abs:end.abs)]
+
+write.csv(filt.dat, 'cached_data/modelinput_dat.csv', row.names = FALSE)
+
