834bce78205e9becd8a89d4a227b6279771c8f1b
Oliver <soliver@usgs.gov>
2017-08-01 11:45:42 -0500

    made project ids all uppercase to ensure merging went well - added both absorbance data grames

--- scripts/2_process/mergeWQ_DOC_ABS.R
+++ scripts/2_process/mergeWQ_DOC_ABS.R
@@ -1,17 +1,14 @@
 # script to merge WQ, DOC, and absorbance data
 
 WQ <- read.csv('cached_data/cleanedWQdata.csv')
+WQ$ProjectID <- toupper(WQ$ProjectID)
 DOC <- read.csv('cached_data/cleanedDOCdata.csv')
+DOC$ProjectID <- toupper(DOC$ProjectID)
 Abs <- read.csv('cached_data/absSlopesResiduals.csv')
-  
+Abs2 <- read.csv('cached_data/tcorrectedAbsData.csv')
+Abs <- merge(Abs, Abs2, 'GRnumber', all.x = TRUE)  
+Abs$ProjectID <- toupper(Abs$ProjectID)
 all.dat <- merge(Abs, DOC, 'ProjectID', all.x = TRUE)
 all.dat <- merge(all.dat, WQ, 'ProjectID', all.x = TRUE)
-
-# drop ProjectID rows with .R
-# fix this in absorbance import later
-
-reps <- grep('.r|dil|dup', all.dat$ProjectID, ignore.case = TRUE)
-
-all.dat <- all.dat[-reps, ]
 
 write.csv(all.dat, "cached_data/mergedWQ_DOC_ABS.csv", row.names = FALSE)
