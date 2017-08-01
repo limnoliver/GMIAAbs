ce1fdaffcc70adf49e0e61d37f730a681400cf84
Oliver <soliver@usgs.gov>
2017-08-01 14:23:59 -0500

    create function where inputs are response and predictors, then bootstraps cv.glmnet using transformed, scaled predictors and responses

--- scripts/3_analyze/lasso.R
+++ scripts/3_analyze/lasso.R
@@ -1,28 +1,43 @@
 library(glmnet)
 library(censReg)
-
+library(caret)
 # get data
-all.dat <- read.csv('cached_data/mergedWQ_DOC_ABS.csv')
+all.dat <- read.csv('cached_data/filteredWQ_DOC_ABS.csv')
 
-# response is name of column
-# predictors is column index for predictors in df
-# log.response <- T/F should the response be log transformed?
-deicer.lasso <- function(df, response, predictors, log.response){
+df <- all.dat
+response <- 'COD'
+predictors <- names(all.dat)[c(3:105)]
 
-# rearrange vars so all IVs are at the end
-  response.index <- grep(response, names(df))
-  df <- df[,c(response, predictors)]
+abs.lasso <- function(df, response, predictors)
+
+# reduce df to just response (first col) and predictors
+response.col <- which(names(df) %in% response)
+predictor.cols <- which(names(df) %in% predictors)
+df <- df[,c(response, predictors)]
+df <- df[complete.cases(df),]
+
+# log transform absorbance IVs
+abs.ivs <- grep('A\\d+', names(df))
+df[,abs.ivs] <- log10(df[,abs.ivs])
   
 # log transform response
-  
+df[,response] <- log10(df[,response])
+
+# scale predictors
+df[,-1] <- scale(df[,-1])
+
+# create matrix for predictor vars
+matIVs = as.matrix(df[,-1])
+colnames(matIVs) <- names(df)[-1]
 
 # write equation
-  equation <- paste(response, "~", names(df[,c(2:length(df))]), sep = " ")
+equation <- paste(response, "~", paste(predictors, collapse = " + "), sep = " ")
   
 
 # find outliers
 
-# deal with censored values
 
-# 
+# run model
+g1<-cv.glmnet(matIVs,y,alpha=1,type.measure="mse",family='gaussian',nlambda=200,standardize=TRUE)
+
 }
