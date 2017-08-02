254da97ae7bf13ce191b1373a16bdb9e76bbc95a
Oliver <soliver@usgs.gov>
2017-08-02 15:33:42 -0500

    changed cutoff to 0.99

--- scripts/3_analyze/remove_corr_vars.R
+++ scripts/3_analyze/remove_corr_vars.R
@@ -1,7 +1,9 @@
 # this script removes correlated variables from
 # remove correlated variables from predictors
-predictors.all <- all.dat[,c(3:105)]
-predictors.cor <- cor(predictors)
-drop.predictors <- findCorrelation(predictors.cor, cutoff = 0.9, verbose = FALSE, exact = TRUE)
+all.dat <- read.csv('cached_data/filteredWQ_DOC_ABS.csv', stringsAsFactors = FALSE)
 
-predictors.keep <- names(predictors)[-drop.predictors]
+predictors.all <- all.dat[,c(3:105)]
+predictors.cor <- cor(predictors.all)
+drop.predictors <- findCorrelation(predictors.cor, cutoff = 0.99, verbose = FALSE, exact = TRUE)
+
+predictors.keep <- names(predictors.all)[-drop.predictors]
