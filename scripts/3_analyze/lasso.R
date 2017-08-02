9095ae620988f43f63986ce805128b0d6de35d25
Oliver <soliver@usgs.gov>
2017-08-02 15:32:13 -0500

    added variable reduction by getting rid of highly correlated variables (0.99) - wrote function to take vals*.5 if they are censored

--- scripts/3_analyze/lasso.R
+++ scripts/3_analyze/lasso.R
@@ -4,16 +4,37 @@ library(caret)
 # get data
 all.dat <- read.csv('cached_data/filteredWQ_DOC_ABS.csv', stringsAsFactors = FALSE)
 
+# remove variables that are highly correlated
+predictors.all <- all.dat[,c(3:105)]
+predictors.cor <- cor(predictors.all)
+drop.predictors <- findCorrelation(predictors.cor, cutoff = 0.99, verbose = FALSE, exact = TRUE)
+predictors.keep <- names(predictors.all)[-drop.predictors]
+
 df <- all.dat
 response <- 'COD'
-predictors <- names(all.dat)[c(3:105)]
+predictors <- predictors.keep
+left.censor.val = 0.5
+right.censor.val = 2
 
-abs.lasso <- function(df, response, predictors)
+abs.lasso <- function(df, response, predictors, censor.val)
+  
+# find out if variable is censored
+remarks <- paste('r', response, sep = '')
+rows.left <- which(df[,remarks]=="<")
+rows.right <- which(df[,remarks]=='>')
+censored <- FALSE
+
+if (length(rows.left)>0|length(rows.right)>0) {
+  censored <- TRUE
+  df[rows.left, response] <- left.censor.val*df[rows.left, response]
+  df[rows.right, response] <- right.censor.val*df[rows.right, response]
+}
 
 # reduce df to just response (first col) and predictors
 response.col <- which(names(df) %in% response)
 predictor.cols <- which(names(df) %in% predictors)
 df <- df[,c(response, predictors)]
+df.complete <- complete.cases(df)
 df <- df[complete.cases(df),]
 
 # log transform absorbance IVs
