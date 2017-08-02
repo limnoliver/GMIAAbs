4dc0650e07d30eca75fd3f77d142a691745687fb
Oliver <soliver@usgs.gov>
2017-08-02 15:32:44 -0500

    cleaned up script

--- scripts/2_process/filter_data.R
+++ scripts/2_process/filter_data.R
@@ -25,5 +25,7 @@ start.abs <- grep('A800', names(filt.dat))
 end.abs <- grep('A503', names(filt.dat))
 filt.dat <- filt.dat[,-(start.abs:end.abs)]
 
+
+
 write.csv(filt.dat, 'cached_data/filteredWQ_DOC_ABS.csv', row.names = FALSE)
 
