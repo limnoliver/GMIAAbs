26a61b2c4c8b77bca95002aa8084e400181ee2dc
Oliver <soliver@usgs.gov>
2017-08-02 15:35:14 -0500

    added script to run glmnet through caret train

---
+++ scripts/3_analyze/caret_glmnet.R
@@ -0,0 +1,46 @@
+library(caret)
+
+# run glmnet with caret
+
+ctrl <- trainControl(method = 'cv',
+                     number = 5,
+                     repeats = 1)
+
+tuning.pars <- list()
+coefs <- data.frame(var.names = predictors,
+                    imp = NA)
+for (i in 1:10){
+  cM <- train(matIVs, y,
+              method="glmnet",
+              trControl = ctrl,
+              tuneLength = 10,
+              preProcess = c('center', 'scale')
+  )
+  tuning.pars[[i]] <- cM$bestTune
+  varimp <- varImp(cM)
+  coefs[,i+1] <- varimp$importance[,1]
+}
+
+
+
+# run all 10 within caret
+
+ctrl <- trainControl(method = 'repeatedcv',
+                     number = 5,
+                     repeats = 10, 
+                     selectionFunction = "oneSE")
+
+cM.all <- train(matIVs, y,
+            method="glmnet",
+            trControl = ctrl,
+            tuneLength = 10,
+            preProcess = c('center', 'scale'))
+
+final.varimp <- varImp(cM)
+
+# get coefficients from final model
+coefs.final <- coef(cM$finalModel, cM$bestTune$lambda)
+
+plot(df$COD~test, xlab = 'Predicted', ylab = 'Observed')
+abline(0,1,col = 'red')
+plot(resid(cM))
