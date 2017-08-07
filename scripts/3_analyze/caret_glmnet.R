834da8cc36949576b8ea7652d5937576c6f31a6a
Oliver <soliver@usgs.gov>
2017-08-07 16:59:09 -0500

    removed loop to iterate through caret train runs

--- scripts/3_analyze/caret_glmnet.R
+++ scripts/3_analyze/caret_glmnet.R
@@ -1,15 +1,17 @@
 library(caret)
 
 # run glmnet with caret
-
-ctrl <- trainControl(method = 'cv',
+run.caret.glmnet <- function(matIVs, y)
+ctrl <- trainControl(method = 'repeatedcv',
                      number = 5,
-                     repeats = 1, 
+                     repeats = 10, 
                      selectionFunction = "oneSE")
 
 tuning.pars <- list()
-coefs <- data.frame(var.names = predictors,
+varimps <- data.frame(var.names = predictors,
                     imp = NA)
+coefs <- data.frame(var.names = c('intercept', predictors),
+                      coef = NA)
 for (i in 1:10){
   cM <- train(matIVs, y,
               method="glmnet",
@@ -19,7 +21,8 @@ for (i in 1:10){
   )
   tuning.pars[[i]] <- cM$bestTune
   varimp <- varImp(cM)
-  coefs[,i+1] <- varimp$importance[,1]
+  varimps[,i+1] <- varimp$importance[,1]
+  coefs[,i+1] <- as.numeric(coef(cM$finalModel, cM$bestTune$lambda))
 }
 
 
@@ -28,7 +31,7 @@ for (i in 1:10){
 
 ctrl <- trainControl(method = 'repeatedcv',
                      number = 5,
-                     repeats = 10, 
+                     repeats = 20, 
                      selectionFunction = "oneSE")
 
 cM.all <- train(matIVs, y,
@@ -37,7 +40,7 @@ cM.all <- train(matIVs, y,
             tuneLength = 10,
             preProcess = c('center', 'scale'))
 
-final.varimp <- varImp(cM.all)
+final.varimp <- varImp(cM.all)$importance[,1]
 
 # get coefficients from final model
 coefs.final <- coef(cM.all$finalModel, cM.all$bestTune$lambda)
@@ -45,3 +48,11 @@ coefs.final <- coef(cM.all$finalModel, cM.all$bestTune$lambda)
 plot(df$COD~test, xlab = 'Predicted', ylab = 'Observed')
 abline(0,1,col = 'red')
 plot(resid(cM))
+
+# use RFE to do variable selection
+ctrl <- rfeControl(functions = "glmnet",
+                    method = 'repeatedcv',
+                     number = 5,
+                     repeats = 10)
+
+profile <- rfe(matIVs, y, refControl = ctrl)
