ac2d31557e5297d9ec8023b7c6866737a97545e4
Oliver <soliver@usgs.gov>
2017-08-08 08:05:04 -0500

    removed old code that iterated through cv.glmnet

--- scripts/3_analyze/lasso.R
+++ scripts/3_analyze/lasso.R
@@ -16,7 +16,7 @@ predictors <- predictors.keep
 left.censor.val = 0.5
 right.censor.val = 2
 
-abs.lasso <- function(df, response, predictors, censor.val)
+abs.lasso <- function(df, response, predictors, left.censor.val = 0.5, right.censor.val = 2)
   
 # find out if variable is censored
 remarks <- paste('r', response, sep = '')
@@ -56,54 +56,3 @@ y = df[,1]
 
 # run bootstrap of glmnet
 mods <- bootstrap.glmnet(n.run = 10)
-
-##########################################################################
-# Sort out variables for Stepwise regressions and run stepwise regressions
-##########################################################################
-
-testvars.orig <- mods[[2]]
-testvars.orig.1se <- mods[[3]]
-modelvars.1se <- mods[[4]]
-varstring.1se <- mods[[5]]
-
-#Determine which model appears most frequently in LASSO cross validation
-unique.1se <- unique(varstring.1se)
-num.occur <- numeric()
-max.occur.1se <- 0
-for (k in 1:length(unique.1se)){
-  num.occur[k] <- length(which(varstring.1se==unique.1se[k]))
-}
-
-model.1se <- which.max(num.occur)
-vars.1se.exists <- !is.na(unique.1se[model.1se])
-
-if(vars.1se.exists){
-  vars.1se <- unlist(strsplit(unique.1se[model.1se],fixed=T,split="+"))
-  form <- formula(paste(response,"~",unique.1se[model.1se]))
-  CR.1se <- censReg(form,left=leftCens,right=rightCens,data=df)
-  
-  #compute LASSO model with CV identified variables
-  form <- formula(paste(response,"~",paste(unique(testvars.orig.1se),collapse="+")))
-  X <- scale(as.matrix(df[,unique(testvars.orig.1se)]))
-  m.CV <-cv.glmnet(X,y,alpha=1,type.measure="mse",family='gaussian',nlambda=200,nfolds=trunc(nrow(X)/3))
-  vars.CVcen <- coef(m.CV)[-1,]
-  vars.CVcen <- vars.CVcen[which(abs(vars.CVcen)>0)]
-  
-  #recalibrate the lasso with censReg
-  form <- formula(paste(response,"~",paste(names(vars.CVcen),collapse="+")))
-  m.CVcen <- censReg(form,left=leftCens,right=rightCens,data=df)
-  
-}
-
-plot(mods[[1]][1])
-# write equation
-equation <- paste(response, "~", paste(predictors, collapse = " + "), sep = " ")
-  
-
-# find outliers
-
-
-# run model
-g1<-cv.glmnet(matIVs,y,alpha=1,type.measure="mse",family='gaussian',nlambda=200,standardize=TRUE)
-
-}
