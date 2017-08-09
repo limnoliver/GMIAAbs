be47f6a18842bf207e8dc62968ed509af54a678c
Oliver <soliver@usgs.gov>
2017-08-09 15:08:40 -0500

    incorporate imputation in lasso model prep

--- scripts/3_analyze/lasso.R
+++ scripts/3_analyze/lasso.R
@@ -11,23 +11,38 @@ drop.predictors <- findCorrelation(predictors.cor, cutoff = 0.99, verbose = FALS
 predictors.keep <- names(predictors.all)[-drop.predictors]
 
 df <- all.dat
-response <- 'COD'
+response <- 'Acetate'
 predictors <- predictors.keep
 left.censor.val = 0.5
 right.censor.val = 2
 
-abs.lasso <- function(df, response, predictors, left.censor.val = 0.5, right.censor.val = 2)
+abs.lasso <- function(df, response, predictors)
   
 # find out if variable is censored
 remarks <- paste('r', response, sep = '')
+remarks <- gsub('rX', 'r', remarks)
 rows.left <- which(df[,remarks]=="<")
-rows.right <- which(df[,remarks]=='>')
-censored <- FALSE
+#rows.right <- which(df[,remarks]=='>')
 
-if (length(rows.left)>0|length(rows.right)>0) {
-  censored <- TRUE
-  df[rows.left, response] <- left.censor.val*df[rows.left, response]
-  df[rows.right, response] <- right.censor.val*df[rows.right, response]
+# impute censored values by assuming a log-normal distribution
+# first, get red of real missing values, and set 
+# censored values to NA
+
+y <- df[, response]
+na.vals <- which(is.na(y))
+y[rows.left] <- NA
+y <- y[-na.vals]
+y <- log(y)
+y <- as.data.frame(y)
+
+# impute values from distribution
+
+if (length(rows.left)>0) {
+  y.imp <- impute.QRILC(as.data.frame(y))[[1]]
+  y <- as.numeric(y.imp)
+
+  }else{
+    y <- as.numeric(y)
 }
 
 # reduce df to just response (first col) and predictors
@@ -43,9 +58,6 @@ sites <- substr(storms, 1, 2)
 abs.ivs <- grep('A\\d+', names(df))
 df[,abs.ivs] <- log10(df[,abs.ivs])
   
-# log transform response
-df[,response] <- log10(df[,response])
-
 # scale predictors
 save.process <- preProcess(df[,-1], method = c('center', 'scale'))
 df[,-1] <- scale(df[,-1])
@@ -54,6 +66,6 @@ df[,-1] <- scale(df[,-1])
 matIVs = as.matrix(df[,-1])
 colnames(matIVs) <- names(df)[-1]
 
-# set response var
-y = df[,1]
+
+}
 
