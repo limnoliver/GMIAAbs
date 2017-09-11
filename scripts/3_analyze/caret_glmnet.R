568a4d581837f03fbbeef3f2dcc7ded0aed86974
Oliver <soliver@usgs.gov>
2017-09-11 15:08:53 -0500

    comment out tolerance measure

--- scripts/3_analyze/caret_glmnet.R
+++ scripts/3_analyze/caret_glmnet.R
@@ -4,7 +4,7 @@ library(caret)
 run.caret.glmnet <- function(matIVs, y) {
 
 # run all 10 within caret
-tolerance2 <- tolerance(x, metric, tol = 5, maximize = FALSE)
+#tolerance2 <- tolerance(x, metric, tol = 5, maximize = FALSE)
 ctrl <- trainControl(method = 'repeatedcv',
                      number = 5,
                      repeats = 10, 
