7cc6fdece1e7970dea315535d31c0283d59f1014
Oliver <soliver@usgs.gov>
2017-09-14 09:59:25 -0500

    modified script to incorporate simpler glmnet output vs caret

--- scripts/3_analyze/viz_model_variability.R
+++ scripts/3_analyze/viz_model_variability.R
@@ -11,114 +11,128 @@ source_url('https://gist.github.com/kdauria/524eade46135f6348140')
 devtools::source_gist("524eade46135f6348140", filename = "ggplot_smooth_func.R")
 
 # grab all alphas and lambdas from first run
-output <- data.frame(matrix(NA, nrow = 7, ncol = 6))
-names(output) <- c('alpha', 'lambda', 'rmse', 'r2.cv', 'r2.train', 'r2.test')
-coefs <- data.frame(matrix(NA, nrow = 15, ncol = 7))
-vars.keep <- list()
+output <- data.frame(matrix(NA, nrow = 7, ncol = 3))
+names(output) <- c('lambda', 'mse', 'r2')
+output2 <- data.frame(matrix(NA, nrow = 7, ncol = 6))
+names(output2) <- c('df_1se', 'df_2se', 'mse_1se', 'mse_2se', 'r2_1se', 'r2_2se')
+coefs_df_stand <- data.frame(matrix(NA, nrow = 15, ncol = 7))
+coefs_df_unstand <- data.frame(matrix(NA, nrow = 15, ncol = 7))
+supp.table <- data.frame(matrix(NA, nrow = 15, ncol = 7))
+
+var.counts <- data.frame(matrix(NA, nrow = 15, ncol = 7))
+pred_obs <- data.frame(matrix(NA, nrow = 1, ncol = 2))
+names(pred_obs) <- c('observed', 's0')
+
+#vars.keep <- list()
 
 for (i in 1:length(responses)){
   temp <- out[[i]]
   response <- responses[i]
   # extract fit stats from first run
-  output[i, 'alpha'] <- temp[[1]][[1]]$alpha
-  output[i, 'lambda'] <- temp[[1]][[1]]$lambda
-  output[i, 'rmse'] <- temp[[5]][1]
-  output[i, 'r2.cv'] <- temp[[4]][1]
+  #output[i, 'alpha'] <- temp[[1]][[1]]$alpha
+  output[i, 'lambda'] <- temp[[10]]
+  output[i, 'mse'] <- temp[[7]]
+  output[i, 'r2'] <- temp[[4]]
+  output2[i, ] <- c(mean(temp[[8]]), mean(temp[[9]]), round(mean(temp[[5]]), 2), round(mean(temp[[6]]),2), 
+                    round(mean(temp[[2]]), 2), round(mean(temp[[3]]), 2))
+  pred_obs <- rbind(pred_obs, temp[[11]])
+  coefs_df_stand[,i] <- temp[[1]]$final_stand
+  coefs_df_unstand[,i] <- temp[[1]]$final_unstand
     
-  train.mod <- lm(temp[[7]][[1]]$train.obs ~ temp[[7]][[1]]$train.pred)
-  output[i, 'r2.train'] <- round(summary(train.mod)$r.squared, 2)
-  
-  test.mod <- lm(temp[[6]][[1]]$hold.obs ~ temp[[6]][[1]]$hold.pred)
-  output[i, 'r2.test'] <- round(summary(test.mod)$r.squared, 2)
-  
+ 
   # create output figs
-  train.dat <- do.call(rbind, temp[[7]])
-  train.dat$iteration <- rep(1:10, each = nrow(temp[[7]][[1]]))
-  temp.name <- paste('figures/holdout_train_', responses[i], '.png', sep = '')
-  temp.x <- paste('log Predicted ', responses.clean[i], sep = '')
-  temp.y <- paste('log Observed ', responses.clean[i], sep = '')
+  #train.dat <- do.call(rbind, temp[[7]])
+  #train.dat$iteration <- rep(1:10, each = nrow(temp[[7]][[1]]))
+  #temp.name <- paste('figures/holdout_train_', responses[i], '.png', sep = '')
+  #temp.x <- paste('log Predicted ', responses.clean[i], sep = '')
+  #temp.y <- paste('log Observed ', responses.clean[i], sep = '')
   
-  r2 <- function(y,x) {round(summary(lm(y~x))$r.squared,2)}
+  #r2 <- function(y,x) {round(summary(lm(y~x))$r.squared,2)}
   
-  r2.train <- train.dat %>%
-    group_by(iteration) %>%
-    summarise(r2.train = r2(train.obs,train.pred))
+  #r2.train <- train.dat %>%
+  #  group_by(iteration) %>%
+  #  summarise(r2.train = r2(train.obs,train.pred))
   
-  r2.train <- r2.train %>%
-    mutate(r2.train.text = paste0('Run ', iteration, ' R2 = ', r2.train))
+  #r2.train <- r2.train %>%
+  #  mutate(r2.train.text = paste0('Run ', iteration, ' R2 = ', r2.train))
 
-  train.dat <- merge(train.dat, r2.train, by = 'iteration')
-  ggsave(temp.name, 
-  ggplot(train.dat, aes(x = train.pred, y = train.obs)) +
-    geom_point(size = 2, shape = 16, color = 'darkgray', alpha = 0.5) +
-    geom_smooth(method = 'lm', formula = y~x) +
-    facet_wrap(~ r2.train.text, scales = 'fixed', nrow = 2) +
-    #geom_text(data = r2.text, aes(label = labels)) +
-    theme_bw() +
-    theme(strip.background = element_blank(), 
-          panel.grid.major = element_blank(), 
-          strip.text = element_text(vjust = -10), 
-          strip.placement = 'outside') +
-    labs(x = temp.x, y = temp.y), 
-  width = 8, height = 4, dpi = 1200)
+  #train.dat <- merge(train.dat, r2.train, by = 'iteration')
+  # ggsave(temp.name, 
+  # ggplot(train.dat, aes(x = train.pred, y = train.obs)) +
+  #   geom_point(size = 2, shape = 16, color = 'darkgray', alpha = 0.5) +
+  #   geom_smooth(method = 'lm', formula = y~x) +
+  #   facet_wrap(~ r2.train.text, scales = 'fixed', nrow = 2) +
+  #   #geom_text(data = r2.text, aes(label = labels)) +
+  #   theme_bw() +
+  #   theme(strip.background = element_blank(), 
+  #         panel.grid.major = element_blank(), 
+  #         strip.text = element_text(vjust = -10), 
+  #         strip.placement = 'outside') +
+  #   labs(x = temp.x, y = temp.y), 
+  # width = 8, height = 4, dpi = 1200)
   
   # test dat
-  test.dat <- do.call(rbind, temp[[6]])
-  test.dat$iteration <- rep(1:10, each = nrow(temp[[6]][[1]]))
-  temp.name <- paste('figures/holdout_test_', responses[i], '.png', sep = '')
-  r2.test <- test.dat %>%
-    group_by(iteration) %>%
-    summarise(r2.test = r2(hold.obs,hold.pred))
+  # test.dat <- do.call(rbind, temp[[6]])
+  # test.dat$iteration <- rep(1:10, each = nrow(temp[[6]][[1]]))
+  # temp.name <- paste('figures/holdout_test_', responses[i], '.png', sep = '')
+  # r2.test <- test.dat %>%
+  #   group_by(iteration) %>%
+  #   summarise(r2.test = r2(hold.obs,hold.pred))
+  # 
+  # r2.test <- r2.test %>%
+  #   mutate(r2.test.text = paste0('Run ', iteration, ' R2 = ', r2.test))
+  # 
+  # test.dat <- merge(test.dat, r2.test, by = 'iteration')
+  # ggsave(temp.name, 
+  #        ggplot(test.dat, aes(x = hold.pred, y = hold.obs)) +
+  #          geom_point(size = 2, shape = 16, color = 'darkgray', alpha = 0.5) +
+  #          geom_smooth(method = 'lm', formula = y~x) +
+  #          facet_wrap(~ r2.test.text, scales = 'fixed', nrow = 2) +
+  #          #geom_text(data = r2.text, aes(label = labels)) +
+  #          theme_bw() +
+  #          theme(strip.background = element_blank(), 
+  #                panel.grid.major = element_blank(), 
+  #                strip.text = element_text(vjust = -10), 
+  #                strip.placement = 'outside') +
+  #          labs(x = temp.x, y = temp.y), 
+  #        width = 8, height = 4, dpi = 1200)
   
-  r2.test <- r2.test %>%
-    mutate(r2.test.text = paste0('Run ', iteration, ' R2 = ', r2.test))
-  
-  test.dat <- merge(test.dat, r2.test, by = 'iteration')
-  ggsave(temp.name, 
-         ggplot(test.dat, aes(x = hold.pred, y = hold.obs)) +
-           geom_point(size = 2, shape = 16, color = 'darkgray', alpha = 0.5) +
-           geom_smooth(method = 'lm', formula = y~x) +
-           facet_wrap(~ r2.test.text, scales = 'fixed', nrow = 2) +
-           #geom_text(data = r2.text, aes(label = labels)) +
-           theme_bw() +
-           theme(strip.background = element_blank(), 
-                 panel.grid.major = element_blank(), 
-                 strip.text = element_text(vjust = -10), 
-                 strip.placement = 'outside') +
-           labs(x = temp.x, y = temp.y), 
-         width = 8, height = 4, dpi = 1200)
-  
-  # create dotplots
-  coefs.dot <- temp[[3]]
-  varimps <- temp[[2]]
-  temp.name <- paste('figures/holdout_coefs.dot_dotplot_', responses[i], '.png', sep = '')
-  png(temp.name, height = 500, width = 500)
-  par(mar = c(5,5,1,1), oma = c(0,4,0,0), mfrow=c(1,2))
-  dotchart2(coefs.dot$coef[2:15], labels = coefs.dot$var.names[2:15], dotsize = 2, xlim = c(min(coefs.dot[2:15, 2:11]),max(coefs.dot[2:15, 2:11])), 
-            xlab = "Coefficients", bty= "L",width.factor = .2, col = rgb(100,100,100,alpha = 100, maxColorValue = 255))
-  for (j in 3:11) {
-    dotchart2(coefs.dot[2:15,j], dotsize = 2, add = TRUE, col = rgb(100,100,100,alpha = 100, maxColorValue = 255))
-  }
-  dotchart2(varimps$imp, labels = '', dotsize = 2, xlim = c(-3,100), 
-            xlab = "Variable Importance", bty= "L",width.factor = .2, col = rgb(100,100,100,alpha = 100, maxColorValue = 255))
-  for (j in 3:11) {
-    dotchart2(varimps[,j], dotsize = 2, add = TRUE, col = rgb(100,100,100,alpha = 100, maxColorValue = 255))
-  }
-  dev.off()
+  # # create dotplots
+  coefs.dot <- temp[[1]][,2:11]
+  # varimps <- temp[[2]]
+  # temp.name <- paste('figures/holdout_coefs.dot_dotplot_', responses[i], '.png', sep = '')
+  # png(temp.name, height = 500, width = 500)
+  # par(mar = c(5,5,1,1), oma = c(0,4,0,0), mfrow=c(1,2))
+  # dotchart2(coefs.dot$coef[2:15], labels = coefs.dot$var.names[2:15], dotsize = 2, xlim = c(min(coefs.dot[2:15, 2:11]),max(coefs.dot[2:15, 2:11])), 
+  #           xlab = "Coefficients", bty= "L",width.factor = .2, col = rgb(100,100,100,alpha = 100, maxColorValue = 255))
+  # for (j in 3:11) {
+  #   dotchart2(coefs.dot[2:15,j], dotsize = 2, add = TRUE, col = rgb(100,100,100,alpha = 100, maxColorValue = 255))
+  # }
+  # dotchart2(varimps$imp, labels = '', dotsize = 2, xlim = c(-3,100), 
+  #           xlab = "Variable Importance", bty= "L",width.factor = .2, col = rgb(100,100,100,alpha = 100, maxColorValue = 255))
+  # for (j in 3:11) {
+  #   dotchart2(varimps[,j], dotsize = 2, add = TRUE, col = rgb(100,100,100,alpha = 100, maxColorValue = 255))
+  # }
+  # dev.off()
   
   # create a column in coefs.dot that sum the number of instances of zero
   zeros <- function(x) {length(which(x == 0))} 
-  zeros.holdout <- apply(coefs.dot[,2:9], 1, zeros)
-  vars.keep[[i]] <- droplevels(coefs.dot$var.names[zeros.holdout == 0])
+  zeros.holdout <- apply(coefs.dot, 1, zeros)
+  var.counts[,i] <- zeros.holdout
+  
+  # create a supplement table that takes unstandardized coefficients and prints the number of zeros
+  supp.table[,i] <- paste0(round(coefs_df_unstand[,i], 1), ' (', zeros.holdout, ')')
+  
+  #vars.keep[[i]] <- droplevels(coefs.dot$var.names[zeros.holdout == 0])
   
   # create table of coefficients by
   # extracting coefficient estimates from 
-  if (i == 1){
-    coefs[,i] <- temp[[3]]$var.names
-    names(coefs)[1] <- 'Predictor Variables'
-  }
-  coefs[,i+1] <- temp[[3]]$coef
-  names(coefs)[i+1] <- responses.clean[i]
+  # if (i == 1){
+  #   coefs[,i] <- temp[[3]]$var.names
+  #   names(coefs)[1] <- 'Predictor Variables'
+  # }
+  # coefs[,i+1] <- temp[[3]]$coef
+  # names(coefs)[i+1] <- responses.clean[i]
   
   # now itiratively remove vars 
   # and run linear model [change to elastic net]
@@ -180,13 +194,19 @@ for (i in 1:length(responses)){
   # dev.off()
 }
 
+supp.table[16:21,] <- t(output2)
+names(supp.table) <- responses.clean 
+supp.table$variable <- c(as.character(temp[[1]]$var.names), names(output2))
+supp.table <- supp.table[,c(8, 1:7)]
 
-output$responses <- responses.clean
-output[,c(2:4)] <- round(output[,c(2:4)], 2)
-coefs[,c(2:8)] = round(coefs[,c(2:8)], 2)
+coef.table <- round(coefs_df_stand, 2)
+names(coef.table) <- responses.clean
+coef.table[16:18, ] <- t(round(output, 2))
+coef.table$variable <- c(as.character(temp[[1]]$var.names), names(output))
+coef.table <- coef.table[,c(8, 1:7)]
 
-write.csv(output, 'cached_data/mod_performance.csv', row.names = F)
-write.csv(coefs, 'cached_data/mod_coefs.csv', row.names = F)
+write.csv(supp.table, 'cached_data/mod_coefs_supp.csv', row.names = F)
+write.csv(coef.table, 'cached_data/mod_coefs.csv', row.names = F)
 library(Hmisc)
 
 
