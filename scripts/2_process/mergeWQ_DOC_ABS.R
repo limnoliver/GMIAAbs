be1cc694fb6b50b8200b80feb5a13d097d344b45
Oliver <soliver@usgs.gov>
2017-12-18 14:21:27 -0600

    modify one water quality point that was failing to merge

--- scripts/2_process/mergeWQ_DOC_ABS.R
+++ scripts/2_process/mergeWQ_DOC_ABS.R
@@ -1,7 +1,9 @@
 # script to merge WQ, DOC, and absorbance data
 
-WQ <- read.csv('cached_data/cleanedWQdata.csv')
+WQ <- read.csv('cached_data/cleanedWQdata.csv', stringsAsFactors = FALSE)
 WQ$ProjectID <- toupper(WQ$ProjectID)
+# change Project ID OUT.S115ST to OUT.S115T - this is an error in the QW data file
+WQ$ProjectID[WQ$ProjectID == "OUT.S115ST"] <- "OUT.S115T"
 DOC <- read.csv('cached_data/cleanedDOCdata.csv')
 DOC$ProjectID <- toupper(DOC$ProjectID)
 Abs <- read.csv('cached_data/absSlopesResiduals.csv')
