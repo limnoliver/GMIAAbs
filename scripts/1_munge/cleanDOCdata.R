b8b04a7a6a9d4ce5363d085378fe948ac153fcf7
Oliver <soliver@usgs.gov>
2017-12-29 15:21:21 -0600

    extract doc qa data and export

--- scripts/1_munge/cleanDOCdata.R
+++ scripts/1_munge/cleanDOCdata.R
@@ -12,6 +12,22 @@ sample.ids <- sample.ids$ProjectID
 # these include blanks and standards
 rows.exclude <- grep('blank*|ppm|unknown|standard*|std|dilution', doc.raw$ProjectID, ignore.case = TRUE)
 doc.cleaned <- doc.raw[-rows.exclude, ]
+doc.qa <- doc.raw[rows.exclude, ]
+
+# clean up doc.qa ProjectIDs a bit
+summary(as.factor(doc.qa$ProjectID))
+doc.qa$ProjectID[grep("1 ppm|1ppm", doc.qa$ProjectID, ignore.case = T)] <- "1 ppm standard"
+doc.qa$ProjectID[grep("10 ppm|10ppm", doc.qa$ProjectID, ignore.case = T)] <- "10 ppm standard"
+doc.qa$ProjectID[grep("20 ppm|20ppm", doc.qa$ProjectID, ignore.case = T)] <- "20 ppm standard"
+doc.qa$ProjectID[grep("blank", doc.qa$ProjectID, ignore.case = T)] <- "blank"
+
+# drop "standards" which don't have any measured value and come from a single date
+# as well as "unknown" samples 
+# as well as "dilution water" samples
+doc.qa <- doc.qa[-grep("stds|standards", doc.qa$ProjectID, ignore.case = T),]
+doc.qa <- doc.qa[-grep("unknown", doc.qa$ProjectID, ignore.case = T),]
+doc.qa <- doc.qa[-grep("dilution water", doc.qa$ProjectID, ignore.case = T),]
+doc.qa <- doc.qa[!is.na(doc.qa$DOC), ]
 
 # get rid of rows with no info in them, either ProjectID or DOC
 doc.cleaned <- doc.cleaned[!is.na(doc.cleaned$DOC), ]
@@ -46,29 +62,37 @@ doc.unique2 <- doc.unique[-reps, ]
 #dat.keep <- which(doc.unique2$ProjectID %in% sample.ids)
 #doc.unique2 <- doc.unique2[dat.keep, ]
 # first, have to format dates properly
-new.dates <- c()
-for (i in 1:length(doc.unique2$Date)){
-  if (nchar(as.character(doc.unique2$Date[i])) == 8 &
-      length(grep('^201', doc.unique2$Date[i])) == 1){
-    new.dates[i] <- as.character(doc.unique2$Date[i])
-  } else if (nchar(as.character(doc.unique2$Date[i])) == 8 &
-             length(grep('^201', doc.unique2$Date[i])) == 0) {
-    new.dates[i] <- paste(substr(as.character(doc.unique2$Date[i]), 5, 8), 
-                          substr(as.character(doc.unique2$Date[i]), 0, 2), 
-                          substr(as.character(doc.unique2$Date[i]), 3, 4), sep = "")
+new.dates.fun <- function(date.vec){
+  new.dates <- c()
+for (i in 1:length(date.vec)){
+  if (nchar(as.character(date.vec[i])) == 8 &
+      length(grep('^201', date.vec[i])) == 1){
+    new.dates[i] <- as.character(date.vec[i])
+  } else if (nchar(as.character(date.vec[i])) == 8 &
+             length(grep('^201', date.vec[i])) == 0) {
+    new.dates[i] <- paste(substr(as.character(date.vec[i]), 5, 8), 
+                          substr(as.character(date.vec[i]), 0, 2), 
+                          substr(as.character(date.vec[i]), 3, 4), sep = "")
     
-  } else if (nchar(as.character(doc.unique2$Date[i])) == 6) {
-    new.dates[i] <- paste('20', substr(as.character(doc.unique2$Date[i]), 5, 6), 
-                          substr(as.character(doc.unique2$Date[i]), 0, 2), 
-                          substr(as.character(doc.unique2$Date[i]), 3, 4), sep = "")
-  } else if (nchar(as.character(doc.unique2$Date[i])) == 9) {
-    new.dates[i] <- gsub('(\\d{8})(.)', '\\1', doc.unique2$Date[i])
+  } else if (nchar(as.character(date.vec[i])) == 6) {
+    new.dates[i] <- paste('20', substr(as.character(date.vec[i]), 5, 6), 
+                          substr(as.character(date.vec[i]), 0, 2), 
+                          substr(as.character(date.vec[i]), 3, 4), sep = "")
+  } else if (nchar(as.character(date.vec[i])) == 9) {
+    new.dates[i] <- gsub('(\\d{8})(.)', '\\1', date.vec[i])
   }
 } 
+  return(new.dates)
+}
 
-doc.unique2$Date_formatted <- new.dates
+new.dates.doc <- new.dates.fun(doc.unique2$Date)
+doc.unique2$Date_formatted <- new.dates.doc
 doc.unique2 <- doc.unique2[,-4]
 doc.unique2 <- unique(doc.unique2)
+
+# get new dates for doc qa data as well
+new.dates.qa <- new.dates.fun(doc.qa$Date)
+doc.qa$Date_formatted <- new.dates.qa
 
 sample.log <- read_xlsx('M:/NonPoint Evaluation/gmia/SLOH labforms and budget/optics/optics.sample.log.ALL.xlsx')
 sample.log <- as.data.frame(sample.log[,c(2,6)])
@@ -102,4 +126,22 @@ doc.unique3$ProjectID <- gsub("-", ".", doc.unique3$ProjectID)
 # 20141202 file
 
 doc.unique3 <- subset(doc.unique3, !(doc.unique3$ProjectID == "US.S111G" & doc.unique3$DOC == 65))
+
+# create QA dataset as well
+# keep only qa values from the same date as samples
+sample.dates <- unique(doc.unique3$Date_formatted)
+doc.qa <- filter(doc.qa, Date_formatted %in% sample.dates)
+doc.qa <- select(doc.qa, ProjectID, DOC, Date_formatted)
+doc.qa <- unique(doc.qa)
+
+# summarize QA data
+doc.qa.summary <- group_by(doc.qa, ProjectID) %>%
+  summarize_at(vars(DOC), funs(mean, median, sd))
+
+# write a qa dataset
+write.csv(doc.qa, "cached_data/QA_DOCdata.csv", row.names = F)
+
+# write a dataset of both qa and samples
+doc.qa <- bind_rows(doc.qa, doc.unique3)
+write.csv(doc.qa, 'cached_data/cleanedDOC_withQA.csv', row.names = FALSE)
 write.csv(doc.unique3, 'cached_data/cleanedDOCdata.csv', row.names = FALSE)
