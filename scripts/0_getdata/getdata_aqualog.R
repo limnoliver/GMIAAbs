3b9fc42255469f22d6c59be92be2eeca2d0e0ef0
Oliver <soliver@usgs.gov>
2017-12-29 15:21:52 -0600

    readability

--- scripts/0_getdata/getdata_aqualog.R
+++ scripts/0_getdata/getdata_aqualog.R
@@ -4,7 +4,8 @@ source("scripts/0_getdata/formatAbsSamplesRevised.R")
 # set home wd
 wd.home <- getwd()
 # collate data
-FinalAbsDf <- formatAbsSamplesRevised(dateLower='20130930',dateUpper='20170610',Type='All',Project='GMIA')
+FinalAbsDf <- formatAbsSamplesRevised(dateLower='20130930',dateUpper='20170610',
+                                      Type='All',Project='GMIA')
 
 # remove columns with NA names
 FinalAbsDf <- FinalAbsDf[,names(FinalAbsDf) != "NA"]
