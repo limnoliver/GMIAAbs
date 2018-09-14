c4bb66079b20d04d618881e2e8c0ae4f5af789bb
Samantha Oliver <soliver@usgs.gov>
2018-09-14 11:12:54 -0500

    had errors in QA munging - some variables did not need and some columns out of order.

--- scripts/1_munge/cleanWQ_QAdata.R
+++ scripts/1_munge/cleanWQ_QAdata.R
@@ -6,13 +6,12 @@ wq.qa <- read_xlsx("raw_data/airport_qw_qaqc.xlsx")
 
 grep("benzotriazole", wq.qa[[1]], ignore.case = T, value = T)
 
-pcodes <- c("00310", "00335", "00367", "91080", "51998", "65240", "63120", "61944")
+pcodes <- c("00310", "00335", "91080", "65240", "63120", "61944")
 pcodes.r <- paste0("R", pcodes)
 pcodes.p <- paste0("P", pcodes)
 pcodes.all <- c(pcodes.r, pcodes.p)
 
-chemicals <- c('COD', "BOD_unfiltered", "BOD_filtered", "PG_unfiltered", "PG_filtered", 
-               "Acetate", "4M-1H-bzt", "5M-1H-bzt")
+chemicals <- c('BOD', "COD", "PG", "Acetate", "4M-1H-bzt", "5M-1H-bzt")
 
 wq.qa <- read_xlsx("raw_data/airport_qw_qaqc.xlsx", skip = 89)
 keep.cols <- names(wq.qa) %in% pcodes.all
@@ -22,9 +21,9 @@ summary(keep.cols)
 
 wq.qa.f <- wq.qa[, keep.cols]
 head(wq.qa.f)
-wq.qa.f <- select(wq.qa.f, STAID:SAMPL, SCMFL, R00310:P91080, R00367:P51998)
-names(wq.qa.f)[seq(from = 8, to = 23, by = 2)] <- paste0(chemicals, "_remark")
-names(wq.qa.f)[seq(from = 9, to = 23, by = 2)] <- chemicals
+wq.qa.f <- select(wq.qa.f, STAID:SAMPL, SCMFL, R00310:P00335, R91080, P91080, R65240, P65240, R63120:P61944)
+names(wq.qa.f)[seq(from = 8, to = 18, by = 2)] <- paste0(chemicals, "_remark")
+names(wq.qa.f)[seq(from = 9, to = 19, by = 2)] <- chemicals
 
 # get rid of site COD
 wq.qa.f <- wq.qa.f[-grep("COD", wq.qa.f$SCMFL),]
